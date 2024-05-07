f6f6e86807f9fe1ad3258191c758ca9be6ad6e80
Yvonne330 <nkem1akonam@gmail.com>
2024-05-07 13:51:44 +0100

    complete

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,24 @@
-"use client"
+"use client";
 
-import { useState } from "react";
-import "./style.css";
 export default function Page(){
-var [money,updateBrowser]=useState(300);
 
-  function addBlock() {
-    money+=400;
-    updateBrowser(money);
-  }
+ function handleClickmenow(){
+   var age = 13+1;
+   alert(age);
 
-function subMoney(){
-    money-=300;
-    updateBrowser(money);
-}
+   age=45;
+   alert(age);
 
+
+    age += 20*2;
+    alert(age);
+ }
 
     return(
-    <main>
-        <h1>My Money App</h1>
-        <p> My Savings for the year so far</p>
-        <h4>NGN {money} </h4>
-        <button className="btn-minus" onClick={subMoney}><i className="fa fa-minus"></i> </button>
-        <button className="btn-plus"   onClick={addBlock}><i className="fa fa-plus"></i></button>
-
-    </main>
-
+        <div>
+            <button onClick={handleClickmenow}>Click Me</button>
+            <p>This will output the number of times the button was clicked</p>
+        </div>
     )
-  
+    
 }
